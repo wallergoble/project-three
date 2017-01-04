6ca8ac18546d17fe9572e84f9f2bcccde48bb800
CHRISTINA PARKHURST <ccparkhurst@users.noreply.github.com>
2017-01-04 15:13:22 -0500

    wired up mongoose

--- app.js
+++ app.js
@@ -5,10 +5,29 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+// Connect to database
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+  mongoose.connect('mongodb://localhost/mean-movies');
+}
+mongoose.connection.on('error', function(err) {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+  }
+);
+mongoose.connection.once('open', function() {
+  console.log("Mongoose has connected to MongoDB!");
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));

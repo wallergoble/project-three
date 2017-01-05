2303390b7042ee2462d392caba9ba4573e91b378
Waller Goble <waller.go@gmail.com>
2017-01-04 19:27:39 -0500

    Wired up app to use movies route

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/stories', stories);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

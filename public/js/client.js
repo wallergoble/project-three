da0bbaaf5ca7219566075655cdc6a8edf412611c
CHRISTINA PARKHURST <ccparkhurst@users.noreply.github.com>
2017-01-06 11:28:13 -0500

    Added states with ui router

--- public/js/client.js
+++ public/js/client.js
@@ -1,12 +1,39 @@
-var myApp = angular.module('storyBook', ['ui.router']);
+'use strict'; // Keeps our code clean
 
-myApp.config(function($stateProvider) {
-	var storyState = {
-		name: 'story',
-		url: '/story',
-		template: '<h1> hello from story template</h1>',
-		controller: 'storyCtrl'
-	}
+const myApp = angular.module('storyBook', ['ui.router']);
 
-	$stateProvider.state(storyState);
+myApp.config(function($stateProvider, $urlRouterProvider) {
+
+	// If the user goes astray, redirect to /home
+	$urlRouterProvider.otherwise('/index');
+
+	$stateProvider
+	.state('index', {
+		url: '/index',
+		template: `
+		<h1> Welcome to our app </h1>
+		<login></login>
+		<p>If you haven't signed up yet, <a ui-sref="/signup"> click here </a> to sign up</p>
+		`
+	})
+	.state('signup', {
+		url: '/signup',
+		template: '<signup></signup>'
+	})
+	.state('storyIndex', {
+		url: '/my-library',
+		template: '<storyIndex></storyIndex>'
+	})
+	.state('storyNew', {
+		url: '/new',
+		template: '<storyNew></storyNew>'
+	})
+	.state('storyShow', {
+		url: '/show/:id',
+		template: '<storyShow></storyShow>'
+	})
+	.state('storyEdit', {
+		url: '/edit/:id',
+		template: '<storyEdit></storyEdit>'
+	});
 });

899c943c2cfc6db3fedcb474f2c46edcbc021127
CHRISTINA PARKHURST <ccparkhurst@users.noreply.github.com>
2017-01-06 12:39:04 -0500

    fixed login component

--- public/js/auth/loginComponent/login.component.js
+++ public/js/auth/loginComponent/login.component.js
@@ -1,6 +1,6 @@
 myApp.component('login', {
   templateUrl: 'js/auth/loginComponent/login.html',
-  controller: function() {
+  controller: function(storyService) {
     console.log('hello from login components controller');
   }
 

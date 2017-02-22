4f25164dee23be05e25f893d1e8b0b0cc3f57e2d
Guest1 <guest1@d25rh0s1gq17im.amer.homedepot.com>
2017-02-22 11:35:14 -0500

    Persist friend names

--- models/story.js
+++ models/story.js
@@ -5,9 +5,35 @@ var StorySchema = new mongoose.Schema({
   name:           { type: String,  required: true },
   animal:         { type: String,  required: true },
   place:          { type: String,  required: true },
-  user: { type: mongoose.Schema.Types.ObjectId, ref:'User'}// added in user for passport and mongoose to use to match users to their stories
-    },
+  user: { type: mongoose.Schema.Types.ObjectId, ref:'User'},// added in user for passport and mongoose to use to match users to their stories
+  friendOne:      { type: String,  required: true }, 
+  friendTwo:      { type: String,  required: true }, 
+},
   { timestamps: true }  // createdAt, updatedAt
 );
 
-module.exports = mongoose.model('Story', StorySchema);
+const Model = mongoose.model('Story', StorySchema);
+
+const newStory = ( story ) =>
+  new Model(Object.assign({}, story, {
+    friendOne: randomName(),
+    friendTwo: randomName(),
+  }))
+
+const randomName = () => names[ ( Math.random() * names.length ) >> 0 ]
+
+let names = ['Perry', 'Philip', 'Christina', 'Katie', 'Stephanie',
+  'Waller', 'Kim', 'Josh', 'Jacob', 'Van', 'Jonathan', 'Jon',
+  'Protik', 'Fahran', 'Maren', 'Dr. Mike', 'Shawn', 'Ben', 'Ed',
+  'Lisa', 'Kenny', 'Ramata', 'Chris', 'Brenden', 'Austin' 
+];
+
+
+module.exports = {
+  Model,
+  newStory,
+}
+
+
+
+
--- public/js/stories/storyShow/storyShow.component.js
+++ public/js/stories/storyShow/storyShow.component.js
@@ -10,20 +10,5 @@ myApp.component('storyShow', {
 			console.log(res.data);
 			this.story = res.data;
 		});
-		
-		// Random name function to add randomness to story
-		this.getRandomNames = function() {
-			let names = ['Perry', 'Philip', 'Christina', 'Katie', 'Stephanie',
-						 'Waller', 'Kim', 'Josh', 'Jacob', 'Van', 'Jonathan', 'Jon',
-						 'Protik', 'Fahran', 'Maren', 'Dr. Mike', 'Shawn', 'Ben', 'Ed',
-						 'Lisa', 'Kenny', 'Ramata', 'Chris', 'Brenden', 'Austin' 
-						];
-			let randomNumber =  Math.floor(Math.random() * names.length);
-			return names[randomNumber]
-		}
-
-
-		this.randomNameOne = this.getRandomNames();
-		this.randomNameTwo = this.getRandomNames();
 	}
 });
